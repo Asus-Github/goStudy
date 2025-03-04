0ec5d03b9c73bf37fcce4b11a1d4a34312387ec5
asus <1572892725@qq.com>
2025-03-04 21:38:34 +0800

    修改demo06

--- Day01/demo06.go
+++ Day01/demo06.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 
 	var num int = 1000
-	var name string = "刘华硕"
+	var name string = "刘华硕123"
 	fmt.Println(num)
 	// Print format 格式化输出
 	fmt.Printf("num = %d\n 地址 = %p\n", num, &num)
