272631f2bca84224ad46f74047efc8ae00259b14
asus <1572892725@qq.com>
2025-03-07 17:47:39 +0800

    if判断

---
+++ Day02/demo20.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num int = 100
+
+	if num < 100 {
+		fmt.Println("hello world")
+	} else {
+		fmt.Println("大于等于100")
+	}
+	fmt.Println("test")
+}
